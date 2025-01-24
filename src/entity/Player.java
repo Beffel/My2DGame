c4350409b85214723bb3cc3a7ead7cefb87a12a7
Raphael Steger <raphaelsteger13@gmail.com>
2025-01-24 14:46:03 +0100

    update
    fixed a bug where the player would receive damage by touching a dying monster

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -297,7 +297,7 @@ public class Player extends Entity {
 
         if (i != 999) {
 
-            if (!invincible) {
+            if (!invincible && !gp.monster[i].dying) {
                 gp.playSE(6);
 
                 int damage = gp.monster[i].attack - defense;
