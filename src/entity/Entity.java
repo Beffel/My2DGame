e6cfad9dcd07c7274edf4a1731e5534e81b005ab
Raphael Steger <raphaelsteger13@gmail.com>
2025-01-24 15:01:46 +0100

    add new projectile and fireball class
    created a new projectile and fireball class

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -47,6 +47,8 @@ public class Entity {
     public int speed;
     public int maxLife;
     public int life;
+    public int maxMana;
+    public int mana;
     public int level;
     public int strength;
     public int dexterity;
@@ -57,11 +59,13 @@ public class Entity {
     public int coin;
     public Entity currentWeapon;
     public Entity currentShield;
+    public Projectile projectile;
 
     // ITEM ATTRIBUTES
     public int attackValue;
     public int defenseValue;
     public String description = "";
+    public int useCost;
 
     // TYPE
     public int type; // 0 = player, 1 = npc, 2 = monster
